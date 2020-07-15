60f7f2b99254ee7f576edca94cdc155589c4308b
Tyler Casperson <tylercasperson@gmail.com>
2020-07-15 00:57:49 -0600

    displays single user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import UserItems from './components/users/UserItems'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <UserItems />
       <h1>Hello from React</h1>
     </div>
   );
