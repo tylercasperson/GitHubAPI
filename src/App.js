bfe685dca49fc72ea84fbebc367435d0de91dbcf
Tyler Casperson <tylercasperson@gmail.com>
2020-07-16 02:12:52 -0600

    added clear functionality to the search bar

--- src/App.js
+++ src/App.js
@@ -17,15 +17,20 @@ class App extends Component {
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`
     );
     this.setState({ users: res.data.items, loading: false });
-    console.log(text);
   };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
           <Users users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ export class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
@@ -19,6 +21,7 @@ export class Search extends Component {
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form className="form" onSubmit={this.onSubmit}>
@@ -35,6 +38,11 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
