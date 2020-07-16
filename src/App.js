0c643360aee0996dc82609b6a20edd6ccf03c770
Tyler Casperson <tylercasperson@gmail.com>
2020-07-16 00:37:50 -0600

    can show more than pre-coded user data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <Users />
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+    console.log(res.data);
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users loading={this.state.users} users={this.state.users} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
