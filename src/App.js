2476bf4c2f5a7fccfbb2b94d5dbade4b77054c35
Tyler Casperson <tylercasperson@gmail.com>
2020-07-15 21:57:27 -0600

    displays multiple users

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import UserItems from './components/users/UserItems'
+import React from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <UserItems />
-      <h1>Hello from React</h1>
+      <div className="container">
+        <Users />
+      </div>
     </div>
   );
 }
