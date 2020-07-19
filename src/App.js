b4355da56001e55a3cfa9a93631894672ae91380
Tyler Casperson <tylercasperson@gmail.com>
2020-07-18 22:25:22 -0600

    added a not found page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import User from "./components/users/User";
-
 import Alert from "./components/layout/Alert";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-
+import NotFound from "./components/pages/NotFound";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
 
@@ -24,6 +23,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" render={(props) => <User />} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
