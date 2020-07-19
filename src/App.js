d218d766096e6de81b0023230a1cc53398cf4f95
Tyler Casperson <tylercasperson@gmail.com>
2020-07-18 22:03:20 -0600

    added a Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import User from "./components/users/User";
-import Users from "./components/users/Users";
-import Search from "./components/users/Search";
+
 import Alert from "./components/layout/Alert";
+import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 
 import GithubState from "./context/github/GithubState";
@@ -21,16 +21,7 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" render={(props) => <User />} />
               </Switch>
